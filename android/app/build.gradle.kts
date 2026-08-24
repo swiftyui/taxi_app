25d0202e89bfe733d6f287937852c706f497331d
Arno van Zyl <arnov2@discovery.co.za>
2026-08-24 09:23:46 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         applicationId = "com.vanzylarno.taxiApp"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        versionCode = 2
         versionName = flutter.versionName
     }
 
